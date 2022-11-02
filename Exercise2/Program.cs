22ee9477dfd0c1dbb5da8cc300bd9302c694471d
Nur Azizah <nurrrazizahhh11@gmail.com>
2022-11-02 16:01:08 +0700

    Menambahkan class ProgramInsertion

--- Exercise2/Program.cs
+++ Exercise2/Program.cs
@@ -1,13 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿    using System;
+    using System.Collections.Generic;
+    using System.Linq;
 
-namespace Exercise2
-{
-    internal class Program
+    namespace Exercise2
     {
-        static void Main(string[] args)
+        ////algoritma yang sesuai dengan intruksi method 1 adalah insertion sort
+        class ProgramInsertion //(kelas untuk algoritma insertion)
+
         {
+            //3 digit nim terakhir : 155
+            //Deklarasi array int dengan ukuran maksimum 45 + 10 + 20 + 20 - 2 * 15 = 65 data
+            //(Maksimal data yang bisa disimpan adalah 65 data)
+            private decimal n;
+
+            //deklarasi variable desimal untuk menyimpan banyaknya data pada array
+            private decimal[] azizah = new decimal[65];
+
+            //membuat fungsi (method) untuk menerima masukan
+            public void Input() // Merupakan method yang hanya melewatkan nilai / tidak mengembalikan nilai
+            {
+
+            }
         }
     }
-}
